2dc7cc251234afae30c5d436d8992724055a6083
Pau Drudis <frikeldon@gmail.com>
2015-04-27 10:47:19 +0000

    Problem 7 solved

---
+++ problem_7/main.js
@@ -0,0 +1,4 @@
+var primes = require('../utils/primes.js');
+
+primes.calculatePrimes(10001);
+console.log(primes.primes[10000]);
---
+++ utils/primes.js
@@ -0,0 +1,54 @@
+(function(exports) {
+
+    var primes = [2, 3];
+    var lastCheck = 5;
+    
+    function checkPrimeNumber(num) {
+    	var max = Math.ceil(Math.sqrt(num));
+    	var length = primes.length;
+    	var current;
+    
+    	for(var i = 0; i < length; i++) {
+    		current = primes[i];
+    		if(num % current === 0) {
+    			return false;
+    		}
+    		if(current > max) {
+    			break;
+    		}
+    	}
+    
+    	return true;
+    }
+    
+    function checkPrimesTo(num) {
+    	for(; lastCheck <= num; lastCheck += 2) {
+    		if(checkPrimeNumber(lastCheck)) {
+    			primes.push(lastCheck);
+    		}
+    	}
+    }
+    
+    function calculatePrimes(amount) {
+    	while(primes.length <= amount) {
+    		if(checkPrimeNumber(lastCheck)) {
+    			primes.push(lastCheck);
+    		}
+    		lastCheck += 2;
+    	}
+    }
+    
+    function isPrime(num) {
+    	if(num >= lastCheck) {
+    		checkPrimesTo(num);
+    	}
+    
+    	return primes.indexOf(num) >= 0;
+    }
+
+    exports.primes = primes;
+    exports.isPrime = isPrime;
+    exports.checkPrimesTo = checkPrimesTo;
+    exports.calculatePrimes = calculatePrimes;
+
+})(typeof exports === 'undefined' ? this['primes']={} : exports);
